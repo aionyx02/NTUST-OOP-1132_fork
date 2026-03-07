4ca94a3cd90a402cf1c7f2376dcc34c88ae97c1a
xinshoutw <me@xinshou.tw>
2026-03-07 17:05:44 +0800

    refactor: optimize Sudoku verification logic and improve input handling

--- TS0504/main.cpp
+++ TS0504/main.cpp
@@ -2,50 +2,51 @@
 /// @brief Verification program that checks if a given 9x9 grid follows Sudoku rules
 ///
 /// @file main.cpp
-/// @author xinshoutw <contact@xinshou.tw>
-/// @date 2025/03/17
-/// @version 0.0.1
+/// @author xinshoutw <me@xinshou.tw>
+/// @date 2026/03/07
+/// @version 0.0.2
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <string>
-#include <vector>
 
-bool verify_sudoku(const std::vector<int>& smap);
+bool verify_sudoku(const uint8_t smap[81]);
 
 int main() {
-    std::vector<int> sudoku_map;
-    sudoku_map.reserve(81);
+    uint8_t sudoku_map[81]{};
     std::string input_str;
 
+    int cur_index = 0;
     while (getline(std::cin, input_str)) {
-        if (input_str.empty()) continue;
+        if (input_str.empty())
+            continue;
 
-        for (const auto& i : input_str) {
-            if (i != ',')
-                sudoku_map.push_back(i - '0' - 1); // map 1 ~ 9 to 0 ~ 8
+        sscanf(
+            input_str.c_str(), "%hhu,%hhu,%hhu,%hhu,%hhu,%hhu,%hhu,%hhu,%hhu\n", sudoku_map + cur_index, &sudoku_map[cur_index + 1], &sudoku_map[cur_index + 2],
+            &sudoku_map[cur_index + 3], &sudoku_map[cur_index + 4], &sudoku_map[cur_index + 5], &sudoku_map[cur_index + 6], &sudoku_map[cur_index + 7],
+            &sudoku_map[cur_index + 8]);
+        cur_index += 9;
 
-            if (static_cast<int>(sudoku_map.size()) == 81) {
-                if (verify_sudoku(sudoku_map)) {
-                    std::cout << "True\n";
-                } else {
-                    std::cout << "False\n";
-                }
-                sudoku_map.clear();
+        if (cur_index == 81) {
+            if (verify_sudoku(sudoku_map)) {
+                std::cout << "True\n";
+            } else {
+                std::cout << "False\n";
             }
+            cur_index = 0;
         }
     }
 }
 
-bool verify_sudoku(const std::vector<int>& smap) {
+bool verify_sudoku(const uint8_t smap[81]) {
     // check horizons
     for (int y = 0; y < 9; ++y) {
         int counter = 0;
         for (int x = 0; x < 9; ++x) {
-            counter += (1 << smap[9 * y + x]);
+            counter |= (1 << smap[9 * y + x]);
         }
 
-        if (counter != 0b111111111)
+        if (counter != 0b1111111110)
             return false;
     }
 
@@ -53,10 +54,10 @@ bool verify_sudoku(const std::vector<int>& smap) {
     for (int x = 0; x < 9; ++x) {
         int counter = 0;
         for (int y = 0; y < 9; ++y) {
-            counter += (1 << smap[9 * y + x]);
+            counter |= (1 << smap[9 * y + x]);
         }
 
-        if (counter != 0b111111111)
+        if (counter != 0b1111111110)
             return false;
     }
 
@@ -64,17 +65,11 @@ bool verify_sudoku(const std::vector<int>& smap) {
     for (int y = 0; y < 9; y += 3) {
         for (int x = 0; x < 9; x += 3) {
             int counter = 0;
-            counter += (1 << smap[9 * y + x]);
-            counter += (1 << smap[9 * y + (x + 1)]);
-            counter += (1 << smap[9 * y + (x + 2)]);
-            counter += (1 << smap[9 * (y + 1) + x]);
-            counter += (1 << smap[9 * (y + 1) + (x + 1)]);
-            counter += (1 << smap[9 * (y + 1) + (x + 2)]);
-            counter += (1 << smap[9 * (y + 2) + x]);
-            counter += (1 << smap[9 * (y + 2) + (x + 1)]);
-            counter += (1 << smap[9 * (y + 2) + (x + 2)]);
+            counter |= (1 << smap[9 * (y + 0) + (x + 0)]) | (1 << smap[9 * (y + 0) + (x + 1)]) | (1 << smap[9 * (y + 0) + (x + 2)]) |
+                       (1 << smap[9 * (y + 1) + (x + 0)]) | (1 << smap[9 * (y + 1) + (x + 1)]) | (1 << smap[9 * (y + 1) + (x + 2)]) |
+                       (1 << smap[9 * (y + 2) + (x + 0)]) | (1 << smap[9 * (y + 2) + (x + 1)]) | (1 << smap[9 * (y + 2) + (x + 2)]);
 
-            if (counter != 0b111111111)
+            if (counter != 0b1111111110)
                 return false;
         }
     }
